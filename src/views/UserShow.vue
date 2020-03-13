7e646809301c5ba5c64d0b01ad0e58d0bbe3715c
ben <benjamin.n.chapin@gmail.com>
2020-03-13 14:45:55 -0500

    adds profile and hikecreate

---
+++ src/views/UserShow.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h2>{{ user.first_name }} {{ user.last_name }}</h2>
+    <p>Email: {{ user.email }}</p>
+    <p>Skill Level: {{ user.skill_level }}</p>
+    <router-link to="/index">Back to Hike Index</router-link>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      photo: {}
+    };
+  },
+  created: function() {
+    axios.get("/api/users/" + this.$route.params.id).then(response => {
+      this.user = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
