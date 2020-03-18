c5f7a2bb6954978489d13ef37a304cad77d8e865
ben <benjamin.n.chapin@gmail.com>
2020-03-18 15:01:13 -0500

    works on hikes show page

--- src/views/HikesShow.vue
+++ src/views/HikesShow.vue
@@ -7,8 +7,34 @@
     <div v-for="waypoint in hike.waypoints">
       <p>Name: {{ waypoint.name }}</p>
       <p>Description: {{ waypoint.description }}</p>
+      <p>Latitude: {{ waypoint.latitude }}</p>
+      <p>Longitude: {{ waypoint.longitude }}</p>
       <p><img :src="waypoint.image_url" alt="" /></p>
+      <button v-on:click="destroyWaypoint(waypoint)">Delete Waypoint</button>
+      <br />
+      <br />
+      <form v-on:submit.prevent="submit()">
+        <h1>Edit Waypoint</h1>
+        <ul>
+          <li class="text-danger" v-for="error in errors">{{ error }}</li>
+        </ul>
+        <div class="form-group">
+          Name:
+          <input type="text" v-model="waypoint.name" />
+          <br />
+          Description:
+          <input type="text" v-model="waypoint.description" />
+          <br />
+          Image URL:
+          <input type="text" v-model="waypoint.image_url" />
+          <br />
+          Address:
+          <input type="text" v-model="waypoint.address" />
+        </div>
+        <button v-on:click="updateWaypoint(waypoint)">Edit Waypoint</button>
+      </form>
     </div>
+    <p>{{ errors }}</p>
     <h1>Add Waypoint To Hike</h1>
     <div>
       Name:
@@ -24,6 +50,7 @@
       <input type="text" v-model="newWaypointAddress" />
       <br />
       <button v-on:click="createWaypoint()">Create Waypoint</button>
+      <br />
     </div>
     <router-link to="/hikes">Back to all hikes</router-link>
   </div>
@@ -42,16 +69,17 @@ export default {
   data: function() {
     return {
       hike: {},
-      waypoints: [],
       newWaypointDescription: "",
       newWaypointName: "",
       newWaypointImageURL: "",
-      newWaypointAddress: ""
+      newWaypointAddress: "",
+      errors: []
     };
   },
   created: function() {
     axios.get("/api/hikes/" + this.$route.params.id).then(response => {
       this.hike = response.data;
+      console.log(response.data);
     });
   },
   methods: {
@@ -59,14 +87,41 @@ export default {
       var params = {
         name: this.newWaypointName,
         description: this.newWaypointDescription,
-        image_url: this.newWaypointImageURL
+        image_url: this.newWaypointImageURL,
+        address: this.newWaypointAddress,
+        hike_id: this.hike.id
       };
-      axios.post("/api/waypoints", params).then(response => {
-        this.waypoints.push(response.data);
-        this.newWaypointImageURL = "";
-        this.newWaypointName = "";
-        this.newWaypointDescription = "";
-        this.newWaypointAddress = "";
+      axios
+        .post("/api/waypoints", params)
+        .then(response => {
+          console.log(response.data);
+          this.hike.waypoints.push(response.data);
+          this.newWaypointImageURL = "";
+          this.newWaypointName = "";
+          this.newWaypointDescription = "";
+          this.newWaypointAddress = "";
+        })
+        .catch(error => {
+          this.errors = error.response.data.errors;
+        });
+    },
+    destroyWaypoint: function(waypoint) {
+      axios.delete("/api/waypoints/" + this.waypoint.id).then(response => {
+        var index = this.hike.waypoints.indexOf(waypoint);
+        this.hike.waypoints.splice(index, 1);
+        console.log("Waypoint deleted.", response.data);
+      });
+    },
+    updateWaypoint: function(waypoint) {
+      var params = {
+        name: this.waypoint.name,
+        description: this.waypoint.description,
+        image_url: this.waypoint.image_url,
+        address: this.waypoint.address
+      };
+      axios.patch("/api/waypoints/" + this.waypoint.id, params).then(response => {
+        this.hike.waypoints.push(response.data);
+        console.log("Waypoint updated.", response.data);
       });
     }
   }
